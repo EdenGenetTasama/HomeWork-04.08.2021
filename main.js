063a3db9edaccd201bc780a70ce9997ee25ee412
EdenGenetTasama <edentasama19@gmail.com>
2021-08-04 22:14:40 +0300

    main.js Upgrade

---
+++ main.js
@@ -0,0 +1,141 @@
+// שאלה ראשונה
+// console כתבו תכנית  אשר קולטת מהמשתמש שני מספרים ומדפיסה את המשתנים ל
+// . אם סכום המשתנים גדול או שווה ל13, הדפיסו "bar-mitzva" ,אם 12 הדפיסו "bat-mitzva" , אחרת הדפיסו "kid".
+
+// var changeOne = Number(prompt("Enter your age"));
+// var changeTwo = Number(prompt("OOOPS,Enter your age"));
+
+// // console.log(changeOne);
+// // console.log(changeTwo);
+
+// var result = changeOne + changeTwo;
+
+// if (result >= 13) {
+//   console.log("bar-mitzva");
+// }
+// else if (result == 12) {
+//   console.log("Bat-mitzva");
+// } else {
+//     console.log("kid");
+// }
+
+//========================================================================================================================//
+
+// שאלה שניה//
+
+// צרו לולאת while שמדפיסה "yes" 100 פעמים.
+// צרו לולאה while  שמדפיסה "no" לפי מספר מהמשתמש.
+// //
+
+// var YesButton=0 ;
+
+// while (YesButton < 100 ){
+//     console.log("yes");
+//     YesButton++
+// }
+
+// var NoButton= Number(prompt("Enter a number"));
+// var i = 0;
+
+// while (i<NoButton) {
+//     console.log("No");
+//     i++
+// }
+
+//========================================================================================================================//
+
+// שאלה שלישית
+
+// צרו לולאת FOR שמדפיסה "javascript is not JAVA" 1000 פעמים.
+
+// for (i=0 ; i< 1000 ; i++) {
+//     console.log(i , "javascript is not JAVA");
+// }
+
+//========================================================================================================================//
+
+// שאלה רביעית
+
+// var userName=prompt("Enter your user Name : ")
+// var userNum= Number(prompt("Enter your number : "))
+
+// for (i=0 ; i<userNum; i++) {
+//     console.log(userName);
+// }
+
+//========================================================================================================================//
+
+// שאלה חמישית
+
+// var userName=prompt("Enter your user Name : ")
+// var userEmail= prompt("Tap your Email: ")
+// var userNum= Number(prompt("Enter your age : "))
+
+// // console.log(`My userName is ${userName} ,\n my email is ${userEmail} ,\n my age ${userNum}.`);
+
+// var numberTwo = Number(prompt("Enter second number : "))
+
+// if (userNum>18) {
+// for (i=0;i<numberTwo; i++) {
+// console.log (i, `My userName is ${userName} ,\n my email is ${userEmail} ,\n my age ${userNum}.`)
+// }
+
+// }
+
+//========================================================================================================================//
+
+// שאלה שלוש עשר
+// צרו מערך אקראי של 6 מספרים ומילים. צרו לולאה שעוברת על המערך ומחליפה את המילים במילה "not_number".
+
+// var NumAndWord = ["eden", 25 , "oshri", 22 , "lior" , 50]
+// for (var i=0; i<NumAndWord.length; i++) {
+//     if (NumAndWord[i]*0 != 0) {
+//         NumAndWord[i]="Not a number"
+
+//     }
+// }
+
+//========================================================================================================================//
+
+// שאלה 14
+// צרו לולאת while שמקבלת מהמשתמש מספר, ומאכלסת מערך של 10 מספרים רק זוגיים.
+
+// var i = 0;
+// var j = 0;
+// var numArr = [];
+// while (i<9) {
+
+// while (i<9) {
+// var NumFromUser= Number(prompt("Type your number : "))
+// if (NumFromUser % 2 ==0 )
+
+// i++
+// }
+
+// }
+
+//========================================================================================================================//
+
+// שאלה 15
+
+// var userSquarHight = Number(prompt("TYPE YOUR HIGHT"));
+// var userSquarwidth = Number(prompt("TYPE YOUR width"));
+
+// var sum = userSquarHight * userSquarwidth;
+
+// console.log(sum);
+
+// var numOne = Number(prompt("Enter First numbr :"));
+// var numTwo = Number(prompt("Enter Second numbr :"));
+// var numThree = Number(prompt("Enter Third numbr :"));
+
+// var numFromUser = numOne + numTwo + numThree;
+
+
+//========================================================================================================================//
+
+// שאלה 16
+
+
+
+
