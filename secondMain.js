9410cd3c144ab54962cc339f7f9f739a0a3b3c3c
EdenGenetTasama <edentasama19@gmail.com>
2021-08-05 13:39:36 +0300

    updrade

---
+++ secondMain.js
@@ -0,0 +1,10 @@
+var userNameOne= prompt ("Enter first name");
+var userNameTwo= prompt ("Enter first name");
+var userNameThree= prompt ("Enter first name");
+var userNameFour= prompt ("Enter first name");
+var userNamefive= prompt ("Enter first name");
+
+
+var allNames = [userNameOne,userNameTwo,userNameThree,userNameFour,userNamefive];
+
+console.log(allNames);
